ced7ac11a7fb1afd3fc089703805d230795763de
Joe Wright <joe@joejag.com>
2014-03-20 07:49:20 +0000

    Erlang week 1 ex 2

--- erlang/week1/week1.erl
+++ erlang/week1/week1.erl
@@ -1,10 +1,10 @@
 -module(week1).
--export([word_count/1, word_count_recursive/1, word_count_recursive_again/1]).
+-export([word_count/1, word_count_recursive/1, counter/0]).
 
 % without recursion
 word_count(String) -> length(string:tokens(String, " ")).
 
-% with recursion
+% 1.1 with recursion
 word_count_recursive(String) -> 
   word_count_recursive(0, string:tokens(String, " ")).
 word_count_recursive(Count, []) -> 
@@ -13,12 +13,11 @@ word_count_recursive(Count, List) ->
   [_Head|Tail] = List,
   word_count_recursive(Count + 1, Tail).
 
-% with recursion, different
-word_count_recursive_again(String) -> 
-  word_count_recursive_again(0, String).
-word_count_recursive_again(Count, [" "|Rest]) ->
-  word_count_recursive_again(Count + 1, Rest);
-word_count_recursive_again(Count, [_|[]]) ->
-  Count;
-word_count_recursive_again(Count, [_|Rest]) ->
-  word_count_recursive_again(Count, Rest).
+% 1.2 Write a function to count to ten using recursion
+
+counter() ->
+  counter(1).
+counter(10) ->
+  10;
+counter(N) ->
+  counter(N+1).
